2937b62b00da587463b7771b35031228a5067834
Kiva Vlad <melnilovv717@gmail.com>
2024-06-16 20:18:50 +0300

    add create events

--- src/app/create-event/index.tsx
+++ src/app/create-event/index.tsx
@@ -1,11 +1,42 @@
-import {memo} from "react";
+import {memo, useState, useEffect, useCallback} from "react";
+import {useAppDispatch} from "../../hooks/use-dispatch";
+import {useAppSelector} from "../../hooks/use-selector";
+import {create} from "../../store/reducers/events";
+import {remind} from "../../store/reducers/session";
 import PageLayout from "../../components/page-layout";
 import CreateEventForm from "../../components/create-event-form";
 
 const CreateEvent: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const {data} = useAppSelector(state => state.user);
+
+  const [eventName, setEventName] = useState<string>('');
+  const [eventType, setEventType] = useState<string>('');
+  const [eventDate, setEventDate] = useState<string>(new Date().toISOString());
+  const [participants, setParticipants] = useState<string>('');
+  const [tag, setTag] = useState<string>('');
+
+  useEffect(() => {
+    remind()
+  }, [])
+
+  const callbacks = {
+    // Создание события
+    onCreate: useCallback(() => {
+      dispatch(create({
+        title: eventName,
+        start_date: eventDate,
+        user: data.id
+      }))
+    }, [eventName, eventDate])
+  }
+
   return (
     <PageLayout title="Событие">
-      <CreateEventForm/>
+      <CreateEventForm eventName={eventName} eventType={eventType} eventDate={eventDate} 
+        participants={participants} tag={tag} setEventName={setEventName} setEventType={setEventType}
+        setEventDate={setEventDate} setParticipants={setParticipants} setTag={setTag} onClick={callbacks.onCreate}
+      />
     </PageLayout>
   )
 }
--- src/store/reducers/events.ts
+++ src/store/reducers/events.ts
@@ -1,4 +1,5 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {PayloadAction, createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {API_BASE_URL} from "../../config";
 import type {IEvent} from "../../types/i-event";
 
 type IState = {
@@ -8,21 +9,76 @@ type IState = {
 }
 
 const initialState: IState = {
-  list: [
-    {id: '1', date: '2024-05-01T01:14:53+03:00', title: 'Курс английского'},
-    {id: '2', date: '2024-05-02T09:14:53+03:00', title: 'Курс английского'},
-    {id: '3', date: '2024-05-03T12:14:53+03:00', title: 'Курс английского'},
-    {id: '4', date: '2024-05-04T14:14:53+03:00', title: 'Курс английского'},
-    {id: '5', date: '2024-05-05T18:14:53+03:00', title: 'Курс английского'},
-  ],
+  list: [],
   error: null,
   waiting: false
 }
 
+// Получение списка событий
+export const load = createAsyncThunk<IEvent[], undefined>(
+  'events/loadEvents',
+  async function() {
+    const response = await fetch(`${API_BASE_URL}/api/events/`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+      }
+    })
+    const data = await response.json();
+    return data;
+  }
+)
+
+// Создание нового события
+export const create = createAsyncThunk<IEvent, IEvent>(
+  'events/createEvent',
+  async function(data) {
+    const response = await fetch(`${API_BASE_URL}/api/events/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+      },
+      body: JSON.stringify(data)
+    })
+    const json = await response.json();
+    return json;
+  }
+)
+
 const eventsSlice = createSlice({
   name: 'events',
   initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      // Получение списка событий 
+      .addCase(load.pending, (state) => {
+        state.waiting = true;
+      })
+      .addCase(load.fulfilled, (state, action: PayloadAction<IEvent[]>) => {
+        state.list = action.payload;
+        state.waiting = false;
+        state.error = null;
+      })
+      .addCase(load.rejected, (state) => {
+        state.error = 'Ошибка получения данных';
+      })
+
+      // Создание нового события
+      .addCase(create.pending, (state) => {
+        state.waiting = true;
+      })
+      .addCase(create.fulfilled, (state, action: PayloadAction<IEvent>) => {
+        state.list.push(action.payload);
+        state.waiting = false;
+        state.error = null;
+      })
+      .addCase(create.rejected, (state) => {
+        state.waiting = false;
+        state.error = 'Ошибка при создании';
+      })
+  },
 })
 
 export default eventsSlice.reducer;
